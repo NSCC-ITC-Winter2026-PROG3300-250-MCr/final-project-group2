6eac0d8eecbb00e4e4813e187263c239645127de
Aaron Hirtle <aaronhirtle85@gmail.com>
2026-02-20 14:51:39 +0000

    Fix lint formatting

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,6 @@
-import { test, expect } from "vitest";
-import { sum } from "./sum";
+import { test, expect } from 'vitest';
+import { sum } from './sum';
 
-test("sum adds numbers", () => {
-  expect(sum(2, 3)).toBe(5);
+test('sum adds numbers', () => {
+    expect(sum(2, 3)).toBe(5);
 });
