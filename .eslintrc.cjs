1482e47c60775947928348da5e623a07bd66950a
Aaron Hirtle <aaronhirtle85@gmail.com>
2026-02-20 14:33:28 +0000

    Fix ESLint parser for ES modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
   env: { node: true, es2022: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   extends: ['eslint:recommended'],
   ignorePatterns: [
     'node_modules/',
