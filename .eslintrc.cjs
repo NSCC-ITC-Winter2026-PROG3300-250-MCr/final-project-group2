7dc64416239c572d1ed32b45adf91707c57ceec5
Aaron Hirtle <aaronhirtle85@gmail.com>
2026-02-20 14:32:14 +0000

    Add ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
-  env: { node: true, browser: true, es2022: true },
-  extends: ["eslint:recommended"],
-  ignorePatterns: ["node_modules/", "dist/", "build/", "out/", ".next/", "coverage/"]
+  env: { node: true, es2022: true },
+  extends: ['eslint:recommended'],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'out/',
+    '.next/',
+    'coverage/',
+    '.eslintrc.*'
+  ]
 };
