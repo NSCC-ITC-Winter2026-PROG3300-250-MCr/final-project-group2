0189e551dc89329466f78bc1e38ff1d6e286e5cd
Aaron Hirtle <aaronhirtle85@gmail.com>
2026-02-20 10:26:00 -0400

    Delete .eslintrc.cjs
    double file creation

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true, browser: true, es2022: true },
-  extends: ["eslint:recommended"],
-  ignorePatterns: ["node_modules/", "dist/", "build/", "out/", ".next/", "coverage/"]
-};
